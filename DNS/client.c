e990c74872673885d8c0a464eb7190a403c1ef0d
Swetha Magesh <swethamag.2000@gmail.com>
2020-09-26 00:49:36 +0530

    dns trial failure

--- DNS/client.c
+++ DNS/client.c
@@ -30,7 +30,7 @@ int main() {
   char BUFFER[Buffer_size];
   while (1) {
     bzero(BUFFER, Buffer_size);
-    printf("Enter hostname (cannonical address), enter exit to stop\n ");
+    printf("\nEnter hostname (cannonical address), enter exit to stop\n ");
     fgets(BUFFER, Buffer_size, stdin);
 int wval = sendto(socketfd, BUFFER, Buffer_size,0,(struct sockaddr*)&server_addr,sizeof(server_addr));
     if (strncmp(BUFFER, "exit",4) == 0) {
@@ -46,11 +46,9 @@ int wval = sendto(socketfd, BUFFER, Buffer_size,0,(struct sockaddr*)&server_addr
       int rval = recvfrom(socketfd, BUFFER, Buffer_size,0,NULL,NULL);
       if (rval < 0) {
         perror("Cannot read from server");
-      } else if (strcmp(BUFFER,"")==0) {
-	    printf("\n Try a more  popular hostname. This is not found in list");  
-      }
+      } 
 	else {
-        printf(" \nServer says IP as: %s", BUFFER);
+        printf("IP is  %s", BUFFER);
       }
    if(strncmp(BUFFER,"exit",4)==0){
 	   break;
--- DNS/server.c
+++ DNS/server.c
@@ -16,7 +16,8 @@
 
 #define Buffer_size 200
 int main() {
-
+  int flag;
+  char response[100],request[100];
   int socketfd = socket(AF_INET, SOCK_DGRAM, 0);
   if (socketfd < 0) {
     perror("\n Socket error");
@@ -32,41 +33,46 @@ int main() {
     perror("\nBinding error");
     exit(1);
   }
-  printf("\n Bind successful");
+  printf("\n Bind successful\n");
   struct sockaddr_in client_addr;
   socklen_t clientlen = sizeof(client_addr);
   char BUFFER[Buffer_size];
-   printf("\n Trying to receive from client ..");
-
-    while (1) {
-	     bzero(BUFFER, Buffer_size);
-
-	    int rval;
- rval=recvfrom(socketfd,BUFFER, Buffer_size,0, (struct sockaddr * ) & client_addr, & clientlen);
-    if(strncmp(BUFFER,"exit",4)==0){
-	    break;
+  printf("\n Trying to receive from client ..\n");
+  fflush(stdout);
+  while (1) {
+    bzero(BUFFER, Buffer_size);
+    bzero(response,100);
+    bzero(request,100);
+    int rval;
+    rval = recvfrom(socketfd, BUFFER, Buffer_size, 0, (struct sockaddr * ) & client_addr, & clientlen);
+    if (strncmp(BUFFER, "exit", 4) == 0) {
+      break;
     }
-			
-    printf("Client wants to know the IP for  %s", BUFFER);
-    char request[100],response[100];
-    request="";
-    response="";
-    puts(request);
-    puts(response);
+    printf("\nClient wants to know the IP for  %s", BUFFER);
+    strcpy(request,BUFFER);
+    bzero(BUFFER,Buffer_size);
+    FILE * fp = fopen("dnsMap", "r");
+    flag = 0;
+    while (!feof(fp)) {
+     
+      char temp[100];
+      
+      fscanf(fp, "%s %s", temp, response);
 
-     FILE* fp=fopen("dnsMap","r");
-    while(!feof(fp)){
-	char temp[100];
-	fgets(fp,temp);
-	if(strcmp(temp,request)==0){
-		fgets(fp,response);
-	        printf("%s",response);	
-	}
+      if (strncmp(temp, request,strlen(temp)) == 0) {
+        flag = 1;
+        printf("\n Found the IP as %s", response);
+      }
 
+    }
+    if (flag == 0) {
+	printf("\n Not found");
+      strcpy(response, "Not found");
+    }
+    printf("\n server is sending this mesg to client %s",response);
+    strcpy(BUFFER,response);
+    int wval = sendto(socketfd, BUFFER, Buffer_size, 0, (struct sockaddr * ) & client_addr, sizeof(client_addr));
 
-    } 
-     int wval = sendto(socketfd, response, strlen(response),0,(struct sockaddr*)&client_addr,sizeof(client_addr));
-        
     if (wval < 0) {
       perror("Write unsuccessful");
     }
